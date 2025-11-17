9d4c355fc5c979582fdb84aa94a2961a1697d5cf
Nicholas Brunhart-Lupo <nicholasbl@icloud.com>
2025-11-16 18:21:26 -0700

    Still working away

--- src/reader.rs
+++ src/reader.rs
@@ -20,6 +20,30 @@ pub struct StructMember {
     pub defined_at: Position,
 }
 
+impl StructMember {
+    fn parse(input: (usize, String)) -> Option<(MemberType, Self)> {
+        let mut iter = make_mem_split(&input);
+        let mem_ty = consume_member_start(&mut iter)?;
+
+        let (place, name) = iter.next()?;
+
+        demand_string(&mut iter, ":");
+
+        let ty = iter.next()?.1.to_string();
+
+        demand_done(iter);
+
+        Some((
+            mem_ty,
+            Self {
+                name: name.into(),
+                ty,
+                defined_at: place,
+            },
+        ))
+    }
+}
+
 /// Plain-old-data aggregate with C-like layout.
 #[derive(Debug)]
 pub struct Pack {
@@ -32,6 +56,31 @@ pub struct EnumMember {
     pub value: i64,
     pub defined_at: Position,
 }
+
+impl EnumMember {
+    fn parse(input: (usize, String)) -> Option<(MemberType, Self)> {
+        let mut iter = make_mem_split(&input);
+        let mem_ty = consume_member_start(&mut iter)?;
+
+        let (place, name) = iter.next()?;
+
+        demand_string(&mut iter, "=");
+
+        let value = iter.next()?.1.parse().unwrap();
+
+        demand_done(iter);
+
+        Some((
+            mem_ty,
+            Self {
+                name: name.into(),
+                value,
+                defined_at: place,
+            },
+        ))
+    }
+}
+
 /// Enum with an explicit primitive underlying type.
 #[derive(Debug)]
 pub struct Enum {
@@ -44,8 +93,35 @@ pub struct Enum {
 pub struct BitfldMember {
     pub name: String,
     pub ty: TypeName,
-    pub range: RangeInclusive<u32>,
+    pub range: String,
     pub defined_at: Position,
+}
+
+impl BitfldMember {
+    fn parse(input: (usize, String)) -> Option<(MemberType, Self)> {
+        let mut iter = make_mem_split(&input);
+        let mem_ty = consume_member_start(&mut iter)?;
+
+        let range = iter.next()?.1.to_string().unwrap();
+
+        let (place, name) = iter.next()?;
+
+        demand_string(&mut iter, ":");
+
+        let ty = iter.next()?.1.to_string();
+
+        demand_done(iter);
+
+        Some((
+            mem_ty,
+            Self {
+                name: name.into(),
+                ty,
+                range,
+                defined_at: place,
+            },
+        ))
+    }
 }
 
 /// Bitfield backed by an integer/enum type with named bit ranges.
@@ -60,6 +136,30 @@ pub struct VariantMember {
     pub ty: TypeName,
     pub value: u64,
     pub defined_at: Position,
+}
+
+impl VariantMember {
+    fn parse(input: (usize, String)) -> Option<(MemberType, Self)> {
+        let mut iter = make_mem_split(&input);
+        let mem_ty = consume_member_start(&mut iter)?;
+
+        let value = iter.next()?.1.parse().unwrap();
+
+        demand_string(&mut iter, "=>");
+
+        let (place, ty) = iter.next()?.1.to_string();
+
+        demand_done(iter);
+
+        Some((
+            mem_ty,
+            Self {
+                ty,
+                value,
+                defined_at: place,
+            },
+        ))
+    }
 }
 
 /// Tagged union where the discriminant has a primitive integer type.
@@ -118,13 +218,8 @@ pub struct Module {
 }
 
 impl Module {
-    pub fn from_stream(source: String, r: &mut impl Read) -> Option<Self> {
-        let mut reader = Reader::new(
-            std::io::BufReader::new(r)
-                .lines()
-                .filter_map(|x| x.ok())
-                .enumerate(),
-        );
+    pub fn from_stream(source: String, stream: String) -> Option<Self> {
+        let mut reader = Reader::new(stream);
 
         let mut module = Module {
             source,
@@ -171,17 +266,18 @@ impl Module {
     }
 }
 
-struct Reader<T: Iterator> {
-    source: Peekable<T>,
+struct Reader {
+    source:
+        Peekable<std::iter::Enumerate<std::io::Lines<std::io::BufReader<std::io::Cursor<String>>>>>,
 }
 
-impl<T> Reader<T>
-where
-    T: Iterator<Item = (usize, String)>,
-{
-    fn new(iter: T) -> Self {
+impl Reader {
+    fn new(s: String) -> Self {
         Self {
-            source: iter.peekable(),
+            source: std::io::BufReader::new(std::io::Cursor::new(s))
+                .lines()
+                .enumerate()
+                .peekable(),
         }
     }
 
@@ -190,7 +286,7 @@ where
             let x = self.source.next();
 
             match x {
-                Some((num, ld)) => {
+                Some((num, Ok(ld))) => {
                     // sanitize
                     let mut string = ld.trim();
 
@@ -204,92 +300,104 @@ where
 
                     return Some((num, string.to_string()));
                 }
-                None => return None,
+                _ => return None,
             }
         }
     }
 
-    fn make_aline_iter(s: &(usize, String)) -> impl Iterator<Item = (Position, &str)> {
-        split_whitespace_indices(&s.1).map(|x| {
-            (
-                Position {
-                    line: s.0,
-                    column: x.0,
-                },
-                x.1,
-            )
-        })
+    fn member_iter<Func, U>(&mut self, mut f: Func) -> Option<(Vec<U>, Option<U>)>
+    where
+        Func: FnMut((usize, String)) -> Option<(MemberType, U)>,
+    {
+        let mut ret = vec![];
+        let mut def = None;
+
+        loop {
+            let l = self.has_member_start()?;
+
+            let Some(item) = f(l) else {
+                break;
+            };
+
+            match item.0 {
+                MemberType::Normal => ret.push(item.1),
+                MemberType::Default => {
+                    assert!(def.is_none());
+                    def = Some(item.1);
+                }
+            }
+        }
+
+        Some((ret, def))
     }
 
     fn has_member_start(&mut self) -> Option<(usize, String)> {
-        let l = self.source.peek()?;
-        if l.1.starts_with("-") {
+        let Some((_, Ok(line))) = self.source.peek() else {
+            return None;
+        };
+        if line.starts_with("-") || line.starts_with("=") {
             self.next_line()
         } else {
             None
         }
     }
 
-    fn consume_member_start<'a>(iter: &mut impl Iterator<Item = (Position, &'a str)>) {
-        Self::consume_string(iter, "-");
-    }
-
-    fn consume_string<'a>(iter: &mut impl Iterator<Item = (Position, &'a str)>, text: &str) {
-        match iter.next() {
-            Some((_, x)) if x == text => {
-                return;
-            }
-            _ => panic!("NOPE"),
-        }
-    }
-
-    fn demand_done<'a>(mut iter: impl Iterator<Item = (Position, &'a str)>) {
-        assert!(matches!(iter.next(), None));
-    }
-
-    fn parse_struct_member(&mut self) -> Option<StructMember> {
-        let Some(next) = self.has_member_start() else {
-            return None;
-        };
-
-        let mut parts = Self::make_aline_iter(&next);
-
-        Self::consume_member_start(&mut parts);
-
-        let name = parts.next()?;
-
-        Self::consume_string(&mut parts, ":");
-
-        let ty = parts.next()?;
-
-        Self::demand_done(parts);
-
-        Some(StructMember {
-            name: name.1.into(),
-            ty: ty.1.into(),
-            defined_at: name.0,
-        })
-    }
-
     fn parse_pack<'a, I>(&mut self, _extra: Option<&str>) -> Option<TypeKind> {
-        let members = std::iter::from_fn(|| self.parse_struct_member()).collect();
+        let (members, default) = self.member_iter(StructMember::parse)?;
+
+        assert!(default.is_none());
 
         Some(TypeKind::Pack(Pack { members }))
     }
 
     fn parse_seq<'a, I>(&mut self, _extra: Option<&str>) -> Option<TypeKind> {
-        let members = std::iter::from_fn(|| self.parse_struct_member()).collect();
+        let (members, default) = self.member_iter(StructMember::parse)?;
+
+        assert!(default.is_none());
 
         Some(TypeKind::Sequence(Sequence { members }))
     }
 
     fn parse_enum<'a, I>(&mut self, extra: Option<&str>) -> Option<TypeKind> {
-        let ty = extra.unwrap();
+        let ty = extra.unwrap().to_string();
 
-        //let members = std::iter::from_fn(|| self.parse_struct_member()).collect();
+        let (members, default) = self.member_iter(EnumMember::parse)?;
 
-        //Some(TypeKind::Sequence(Sequence { members }))
+        Some(TypeKind::Enum(Enum {
+            ty,
+            members,
+            default,
+        }))
     }
+
+    fn parse_bitfld<'a, I>(&mut self, extra: Option<&str>) -> Option<TypeKind> {
+        let ty = extra.unwrap().to_string();
+
+        let (members, default) = self.member_iter(BitfldMember::parse)?;
+
+        assert!(default.is_none());
+
+        Some(TypeKind::Bitfld(Bitfld { ty, members }))
+    }
+
+    fn parse_variant<'a, I>(&mut self, extra: Option<&str>) -> Option<TypeKind> {
+        let ty = extra.unwrap().to_string();
+
+        let (members, default) = self.member_iter(VariantMember::parse)?;
+
+        assert!(default.is_none());
+
+        Some(TypeKind::Variant(Variant {
+            ty,
+            members,
+            default,
+        }))
+    }
+}
+
+enum MemberType {
+    Normal,
+    Default,
 }
 
 fn addr_of(s: &str) -> usize {
@@ -299,4 +407,41 @@ fn addr_of(s: &str) -> usize {
 fn split_whitespace_indices(s: &str) -> impl Iterator<Item = (usize, &str)> {
     s.split_whitespace()
         .map(move |sub| (addr_of(sub) - addr_of(s), sub))
+}
+
+fn make_mem_split(input: &(usize, String)) -> impl Iterator<Item = (Position, &str)> {
+    split_whitespace_indices(&input.1).map(|x| {
+        (
+            Position {
+                line: input.0,
+                column: x.0,
+            },
+            x.1,
+        )
+    })
+}
+
+fn consume_member_start<'a>(
+    iter: &mut impl Iterator<Item = (Position, &'a str)>,
+) -> Option<MemberType> {
+    let n = iter.next()?;
+
+    match n.1 {
+        "-" => Some(MemberType::Normal),
+        ">" => Some(MemberType::Default),
+        _ => None,
+    }
+}
+
+fn demand_string<'a>(iter: &mut impl Iterator<Item = (Position, &'a str)>, text: &str) {
+    match iter.next() {
+        Some((_, x)) if x == text => {
+            return;
+        }
+        _ => panic!("NOPE"),
+    }
+}
+
+fn demand_done<'a>(mut iter: impl Iterator<Item = (Position, &'a str)>) {
+    assert!(matches!(iter.next(), None));
 }
